8a1d2fba5777906186f0cc4f619ecf13bfdd6549
bird348 <bird348@gmail.com>
2021-09-06 18:16:52 +0700

    edit index.js

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,11 @@
 import Layout from "../components/Layout"
-import { Text } from "@chakra-ui/react"
 
-
-export default function Home({ blogs }) {
+export default function Home() {
   
   return (
     <Layout title="KKBlog">
-      <ul>
-         {blogs.map((blog) => {
-           return(
-              <li key={blog.blogId}><Text color="black">{ blog.title }</Text></li>
-           )
-         })}
-      </ul>
-     
+      <p style={{textAlign: "center"}}>พบกันเร็วๆ นี้</p>
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3001/blogs")
-  const blogs = await res.json()
-  
-  if (!blogs) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: {
-      blogs,
-    }
-  }
-}
